1764db4474373d1eb7c900dec73978ad218cd3c9
Mario Reder <mario.reder@pm.me>
2025-09-30 10:52:13 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "𝕏, https://x.com/AstralDEX",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
